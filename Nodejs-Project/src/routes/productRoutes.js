97c17a7b6cba089308cd0e114a984a1c00218f77
youssef atef mohamed <120659386+youssef1232004@users.noreply.github.com>
2026-03-20 01:20:49 +0200

    Refactor product controller import path

--- Nodejs-Project/src/routes/productRoutes.js
+++ Nodejs-Project/src/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const productController = require("../controllers/productController");
+const productController = require("../controllers/product.controller");
 const validateRequest = require("../middlewares/validateHandler");
 const {
   productValidationRules,
