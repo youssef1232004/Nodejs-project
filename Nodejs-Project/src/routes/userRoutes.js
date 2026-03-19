b7588e94eab3a5568c742ec7c1387f7c8fec8980
youssef atef mohamed <120659386+youssef1232004@users.noreply.github.com>
2026-03-20 01:20:34 +0200

    Refactor userController import path in userRoutes

--- Nodejs-Project/src/routes/userRoutes.js
+++ Nodejs-Project/src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userController");
+const userController = require("../controllers/user.controller");
 const validateRequest = require("../middlewares/validateHandler");
 const {
   userValidationRules,
